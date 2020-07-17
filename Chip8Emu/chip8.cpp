c612eb68c96c133cd014a29e5024e4e2c4b25db9
Jaime Garcia Jr <jjgarcia2725@gmail.com>
2020-07-17 17:49:00 -0500

    added OP_Dxyn function

--- Chip8Emu/chip8.cpp
+++ Chip8Emu/chip8.cpp
@@ -386,3 +386,38 @@ void Chip8::OP_Cxkk()
 	// generate a random byte and assign it to the register
 	registers[Vx] = randByte(randGen) & byte;
 }
+
+// Function to display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision
+void Chip8::OP_Dxyn() {
+	uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+	uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+	uint8_t height = opcode & 0x000Fu;
+
+	uint8_t xPos = registers[Vx] % VIDEO_WIDTH;
+	uint8_t yPos = registers[Vy] % VIDEO_HEIGHT;
+
+	// VF register, register 15
+	registers[0xF] = 0;
+
+	for (unsigned int row = 0; row < height; ++row) {
+		uint8_t spriteByte = memory[index + row];
+
+		for (unsigned int col = 0; col < 8; ++col) {
+			uint8_t spritePixel = spriteByte & (0x80u >> col);
+			uint32_t* screenPixel = &display[(yPos + row) * VIDEO_WIDTH + (xPos + col)];
+
+			// if sprite exists
+			if (spritePixel) {
+
+				// if this is true, it means collision is occuring 
+				if (*screenPixel == 0xFFFFFFFF) {
+					// sets VF to 1
+					registers[0xF] = 1;
+				}
+
+				// XOR sprite
+				*screenPixel ^= 0xFFFFFFFF;
+			}
+		}
+	}
+}
--- Chip8Emu/chip8.h
+++ Chip8Emu/chip8.h
@@ -13,6 +13,13 @@
 
 using namespace std;
 
+const unsigned int KEY_COUNT = 16;
+const unsigned int MEMORY_SIZE = 4096;
+const unsigned int REGISTER_COUNT = 16;
+const unsigned int STACK_LEVELS = 16;
+const unsigned int VIDEO_HEIGHT = 32;
+const unsigned int VIDEO_WIDTH = 64;
+
 // Chip8 class
 class Chip8 {
 	public:
@@ -22,6 +29,9 @@ class Chip8 {
 
 		// Chip8 function to load in a given ROM from a filename
 		void LoadROM(char const* filename);
+
+		uint32_t display[VIDEO_WIDTH * VIDEO_HEIGHT]{};	// 32-bit display for output
+		uint8_t keys[KEY_COUNT];						// 8-bit array for key inputs
 
 	private:
 
@@ -95,19 +105,21 @@ class Chip8 {
 
 		// Set Vx = random byte AND kk.
 		void OP_Cxkk();
+
+		// Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision
+		void OP_Dxyn();
 	
 
 		// Chip-8 emulator specfications as listed here: https://austinmorlan.com/posts/chip8_emulator/
-		uint8_t memory[4096]{};			// creates memory array composed of 8-bit elements
-		uint8_t registers[16]{};		// creates 16 8-bit registers for the emulator
+		uint8_t memory[MEMORY_SIZE]{};			// creates memory array composed of 8-bit elements
+		uint8_t registers[REGISTER_COUNT]{};	// creates 16 8-bit registers for the emulator
 		uint8_t stack_pointer{};		// 8-bit variable for stack pointer
-		uint8_t keys[16];				// 8-bit array for key inputs
 		uint8_t delayTimer{};			// 8-bit delay timer
 		uint8_t soundTimer{};			// 8-bit sound timer
 		uint16_t index{};				// 16-bit index variable
 		uint16_t program_counter{};		// 16-bit program_counter variable
-		uint16_t stack[16]{};			// creates 16-bit memory stack array
+		uint16_t stack[STACK_LEVELS]{};	// creates 16-bit memory stack array
 		uint16_t opcode;				// 16-bit opcode instruction
-		uint32_t display[64 * 32]{};	// 32-bit display for output
+		
 
 };
